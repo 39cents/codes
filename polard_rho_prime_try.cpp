3d60af726d451aa10ba8259d7984a6d62bc32532
39cents <48502947+39cents@users.noreply.github.com>
2019-03-13 08:45:32 +0530

    Add files via upload

---
+++ polard_rho_prime_try.cpp
@@ -0,0 +1,53 @@
+//try this it is better than the other polard function
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long int l;
+
+l gcd(l x,l n)
+{
+	l c;
+	while(n)
+	{
+		c=x%n;
+		x=n;
+		n=c;
+	}
+	return x;
+	
+}
+
+l f(l a , l n)
+{
+	l a1=rand()%n+2;
+	l result= (a*a+a1)%n;
+	return result;
+	
+}
+l polardrho(l n)
+{
+	srand(time(NULL));		//initailizing pseudo random generator
+	if(n==1) return 1;
+	if(n%2==0) return 2;
+	l a=rand()%n+2;
+	l b=a;
+	l d=1;
+	while(d==1)
+	{
+		a=f(a,n);
+		b=f(f(b,n),n);
+		d=gcd(abs(a-b),n);
+		if(d>1) return d;	
+	}
+}
+int main()
+{
+	l input;cin>>input;
+	while(input>1)
+	{
+		l res=polardrho(input);
+		input=input/res;
+		cout<<res<<" ";
+	}
+
+}
